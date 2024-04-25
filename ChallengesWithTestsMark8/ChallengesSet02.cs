8fde7ab96347efa63f81d97970ff599650773dbb
mark aldridge <mark.aldridge1@protonmail.com>
2024-04-24 17:05:10 -0700

    Challenge Set 2 check in

--- ChallengesWithTestsMark8/ChallengesSet02.cs
+++ ChallengesWithTestsMark8/ChallengesSet02.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,52 +9,132 @@ namespace ChallengesWithTestsMark8
     {
         public bool CharacterIsALetter(char c)
         {
-            throw new NotImplementedException();
+            string alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            foreach (char letter in alphabet)
+            {
+                if (letter == c)
+                {
+                    return true;
+                }
+            }
+            return false;
+            //return char.IsLetter(c);
         }
 
         public bool CountOfElementsIsEven(string[] vals)
         {
-            throw new NotImplementedException();
+            if (vals.Length % 2 == 0)
+            {
+                return true;
+            }
+            return false;
         }
 
         public bool IsNumberEven(int number)
         {
-            throw new NotImplementedException();
+            if (number % 2 == 0)
+            {
+                return true;
+            }
+            return false;
         }
 
         public bool IsNumberOdd(int num)
         {
-            throw new NotImplementedException();
+            var div = num % 2;
+            if (num % 2 != 0)
+            {
+                return true;
+            }
+            return false;
         }
 
         public double SumOfMinAndMax(IEnumerable<double> numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null || numbers.Count() == 0)
+            {
+                return 0;
+            } else
+            {
+                return numbers.Min() + numbers.Max();
+            }
         }
 
         public int GetLengthOfShortestString(string str1, string str2)
         {
-            throw new NotImplementedException();
+            int res = 0;
+            if (str1.Length > str2.Length)
+            {
+                return res = str2.Length;
+            } else
+            {
+                return str1.Length;
+            }
         }
 
         public int Sum(int[] numbers)
         {
-            throw new NotImplementedException();
+            int result = 0;
+            if (numbers == null || numbers.Length == 0)
+            {
+                return result;
+            } else
+            {
+                foreach (var number in numbers)
+                {
+                    result += number;
+                }
+                return result;
+            }
         }
 
         public int SumEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+            int result = 0;
+            if (numbers == null || numbers.Length == 0)
+            {
+                return result;
+            } else
+            {
+                foreach (var number in numbers)
+                {
+                    if (number % 2 == 0)
+                    {
+                        result += number;
+                    }
+                }
+                return result;
+            }
         }
 
         public bool IsSumOdd(List<int> numbers)
         {
-            throw new NotImplementedException();
+            var result = 0;
+            if (numbers == null || numbers.Count() == 0)
+            {
+                return false;
+            }
+            else
+            {
+                foreach (var number in numbers)
+                {
+                    result += number;
+                }
+                return result % 2 != 0;
+            }
         }
 
         public long CountOfPositiveOddsBelowNumber(long number)
         {
-            throw new NotImplementedException();
+            long res = 0;
+            for (var i = 0; i < number; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    res++;
+                }
+            }
+            return res;
         }
     }
 }
