75d4e29cfe8a5b7b8e8bfb86c1ffd05168ebd5ea
mark aldridge <mark.aldridge1@protonmail.com>
2024-04-30 15:25:46 -0700

    Challenges3 check-in

--- ChallengesWithTestsMark8/ChallengesSet03.cs
+++ ChallengesWithTestsMark8/ChallengesSet03.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,47 +6,124 @@ namespace ChallengesWithTestsMark8
     {
         public bool ArrayContainsAFalse(bool[] vals)
         {
-            throw new NotImplementedException();
+            foreach (var val in vals)
+            {
+                if (!val) return true;
+            }
+            return false;
         }
 
         public bool IsSumOfOddsOdd(IEnumerable<int> numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null) return false;
+            var sum = 0;
+            foreach (int num in numbers)
+            {
+                if (num % 2 != 0)
+                {
+                    sum += num;
+                }
+            }
+            if (sum % 2 != 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public bool PasswordContainsUpperLowerAndNumber(string password)
         {
-            throw new NotImplementedException();
+            //return password.Any(char.IsUpper) && password.Any(char.IsLower) && password.Any(char.IsNumber);
+            string lettersUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string lettersLower = "abcdefghijklmnopqrstuvwxyz";
+            string numbers = "1234567890";
+            bool hasLettersUpper = false;
+            bool hasLettersLower = false;
+            bool hasNumbers = false;
+            foreach (char c in password)
+            {
+                if (lettersUpper.Contains(c))
+                {
+                    hasLettersUpper = true;
+                }
+                if (lettersLower.Contains(c))
+                {
+                    hasLettersLower = true;
+                }
+                if (numbers.Contains(c))
+                {
+                    hasNumbers = true;
+                }
+            }
+            if (hasLettersUpper && hasLettersLower && hasNumbers)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public char GetFirstLetterOfString(string val)
         {
-            throw new NotImplementedException();
+            return char.Parse(val.Substring(0, 1));
+            //return val[0];
+            //return val.FirstOrDefault();
         }
 
         public char GetLastLetterOfString(string val)
         {
-            throw new NotImplementedException();
+            return char.Parse(val.Substring(val.Length - 1, 1));
         }
 
         public decimal Divide(decimal dividend, decimal divisor)
         {
-            throw new NotImplementedException();
+            if (divisor == 0) return 0;
+            return dividend / divisor;
         }
 
         public int LastMinusFirst(int[] nums)
         {
-            throw new NotImplementedException();
+            return nums[nums.Length - 1] - nums[0];
         }
 
         public int[] GetOddsBelow100()
         {
-            throw new NotImplementedException();
+            int[] result = new int[50];
+            int counter = 0;
+            for (int i = 0; i <= 100; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    result[counter] = i;
+                    counter++;
+                }
+            }
+            return result;
         }
 
         public void ChangeAllElementsToUppercase(string[] words)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < words.Length; i++)
+            {
+                words[i] = words[i].ToUpper();
+            }
+            //string[] result = new string[words.Length];
+            //string temp;
+            //for (int i = 0; i < words.Length; i++)
+            //{
+            //    temp = "";
+            //    foreach (char c in words[i])
+            //    {
+            //        temp += char.IsLetter(c) ? c.ToString().ToUpper() : c.ToString();
+            //    }
+            //    result[i] = temp;
+            //}
+            //return result;
         }
     }
 }
